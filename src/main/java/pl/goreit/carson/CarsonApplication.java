f398aa9deea89484506b4a1066e0c87ba4e8e613
goreit <p.szczepkowski87@gmail.com>
2020-12-04 12:38:56 +0100

    Initial commit

---
+++ src/main/java/pl/goreit/carson/CarsonApplication.java
@@ -0,0 +1,13 @@
+package pl.goreit.carson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsonApplication.class, args);
+	}
+
+}
